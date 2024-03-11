5a3d9d0fcd117a4eb0b042389b617b163bbd9244
nguyenbinhduong108 <binhduong10082002@gmail.com>
2024-03-11 22:09:49 +0700

    useState hook and event handle

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,3 +1,34 @@
+import { useState } from "react";
+
 export default function Message() {
-  return <button>Click me</button>;
+  const [index, setIndex] = useState(0);
+  const [incremenBy, setIncremenBy] = useState(1);
+
+  function increment() {
+    setIndex((index) => index + incremenBy);
+  }
+
+  function decrement() {
+    setIndex((index) => index - incremenBy);
+  }
+
+  function increaseIncrement() {
+    setIncremenBy((incremenBy) => incremenBy + 1);
+  }
+
+  function decreaseIncrement() {
+    setIncremenBy((incremenBy) => incremenBy - 1);
+  }
+
+  return (
+    <div>
+      <button onClick={() => increment()}>Increment</button>
+      <button onClick={() => decrement()}>Decrement</button>
+      <h1>Index value:{index}</h1>
+
+      <button onClick={() => increaseIncrement()}>Icrease increment</button>
+      <button onClick={() => decreaseIncrement()}>Decrease increment</button>
+      <h1>Index value:{incremenBy}</h1>
+    </div>
+  );
 }
