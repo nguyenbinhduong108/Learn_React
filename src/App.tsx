266a8d50fcd1eaab50885d818682a370f267669d
nguyenbinhduong108 <binhduong10082002@gmail.com>
2024-03-28 23:43:43 +0700

    useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 import Todo from "./components/Todo";
 import style from "./App.module.css";
-import { useCallback, useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useReducer, useRef, useState } from "react";
+
+const initialState = 0;
+
+const UP_ACTION = "up";
+const DOWN_ACTION = "down";
+
+const reducer = (state: number, action: string) => {
+  // console.log("reducer");
+  switch (action) {
+    case UP_ACTION:
+      return state + 1;
+    case DOWN_ACTION:
+      return state - 1;
+    default:
+      throw new Error("Invalid action");
+  }
+};
 
 export default function App() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  // const inputRef = useRef<HTMLInputElement>(null);
 
-  function handleClick() {
-    inputRef.current?.focus();
-    console.log("focus");
-  }
+  // function handleClick() {
+  //   inputRef.current?.focus();
+  //   console.log("focus");
+  // }
 
   // useEffect(() => {
   //   inputRef.current?.focus();
@@ -16,6 +33,7 @@ export default function App() {
   // });
 
   // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, initialState);
 
   //#region useCallback
   // const onIncrease = useCallback(() => {
@@ -23,13 +41,19 @@ export default function App() {
   // }, []);
 
   return (
-    <div className={style.container}>
-      {/* <Todo /> 
-      <Content onIncrease={onIncrease} />
+    // <div className={style.container}>
+    //   <Todo />
+    //   <Content onIncrease={onIncrease} />
 
-      <h1>{count}</h1>*/}
-      <h1 onClick={handleClick}>Hello</h1>
-      <input ref={inputRef} placeholder="Enter name..." />
+    //   <h1>{count}</h1>
+    //   <h1 onClick={handleClick}>Hello</h1>
+    //   <input ref={inputRef} placeholder="Enter name..." />
+    // </div>
+
+    <div className={style.container}>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(UP_ACTION)}>Up</button>
+      <button onClick={() => dispatch(DOWN_ACTION)}>Down</button>
     </div>
   );
 }
