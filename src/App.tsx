a85911caeae9a6467033d227202138596c0959e5
nguyenbinhduong108 <binhduong10082002@gmail.com>
2024-03-28 22:56:44 +0700

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
 import Todo from "./components/Todo";
 import style from "./App.module.css";
-import Content from "./Content";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const inputRef = useRef<HTMLInputElement>(null);
 
-  const onIncrease = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, []);
+  function handleClick() {
+    inputRef.current?.focus();
+    console.log("focus");
+  }
+
+  // useEffect(() => {
+  //   inputRef.current?.focus();
+  //   console.log(inputRef.current);
+  // });
+
+  // const [count, setCount] = useState(0);
+
+  //#region useCallback
+  // const onIncrease = useCallback(() => {
+  //   setCount((prev) => prev + 1);
+  // }, []);
 
   return (
     <div className={style.container}>
-      {/* <Todo /> */}
+      {/* <Todo /> 
       <Content onIncrease={onIncrease} />
 
-      <h1>{count}</h1>
+      <h1>{count}</h1>*/}
+      <h1 onClick={handleClick}>Hello</h1>
+      <input ref={inputRef} placeholder="Enter name..." />
     </div>
   );
 }
